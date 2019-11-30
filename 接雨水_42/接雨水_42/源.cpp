1de6190c446854dd45c733f30c10c5b23a9ed5f1
QinHaoChen97 <201630257288@mail.scut.edu.cn>
2019-11-30 17:31:33 +0800

    看了官方的题解一次过了，第一次用栈，多练练吧= =，我肚子饿了

--- 接雨水_42/接雨水_42/源.cpp
+++ 接雨水_42/接雨水_42/源.cpp
@@ -19,9 +19,41 @@
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 */
 
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+//题解参考https://leetcode-cn.com/problems/trapping-rain-water/solution/jie-yu-shui-by-leetcode/
+//使用栈的方法求解
+using namespace std;
 class Solution {
 public:
 	int trap(vector<int>& height) {
+		stack<int> wall;
+		int current = 0; //用于存储当前指向的位置，进栈的时候也存入该值，方便计算两墙之间的距离
+		int sum = 0;//存放雨水
+		while (current<height.size())
+		{
+			while (!wall.empty() && height[current]>height[wall.top()]) //数组非空且当前指向的挡板高度大于栈顶挡板高度，由于前提是数组非空，所以不用担心访问越界
+				//当这种情况出现时，1 有可能是两挡板中出现了一矮挡板，2 也有可能是只有一边高挡板的情况
+			{
+				int tp = height[wall.top()];
+				wall.pop(); //将栈顶出栈
+				if (wall.empty())//排除上面的情况二
+				{
+					break;
+				}
+				int distance = current - wall.top()-1; //得到两挡板之间的距离
+				sum += (min(height[wall.top()], height[current]) - tp)*distance;//两墙中较低的墙减去两挡板中较矮的地方*两挡板间的距离
+
+			}
+			wall.push(current++);//即push（current）;current+1;  因为current++返回的是未自加前的结果
+		}
+		return sum;
 
 	}
 };
+int main()
+{
+
+}
